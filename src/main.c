b051c0f0d1dc39af39ff1af950d62af0c3ccb6a7
Ronaldo Evangelista <ronaldinho.evangelista@gmail.com>
2018-04-13 01:05:09 -0300

    no message

--- src/main.c
+++ src/main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <conio2.h>
+#include <malloc.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+
+void linhas();
 
 int main(int argc, char *argv[])
 {
-  
-  system("PAUSE");	
-  return 0;
+	
+	clrscr();
+	textbackground(BLACK);       
+	linhas();  
+	system("pause");
+	return 0;
+}
+
+void linhas(){
+gotoxy(16,8); printf("-----------------------------------------------");   
+gotoxy(16,20);printf("-----------------------------------------------");  
 }
